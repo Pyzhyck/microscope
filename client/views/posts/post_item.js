2e4c3f899722b62ab0e2885cfb2cd46f1b6f94fa
Vitalik <pyzhyck@gmail.com>
2017-02-10 14:37:23 +0200

    Create manager for postItem template

---
+++ client/views/posts/post_item.js
@@ -0,0 +1,9 @@
+/* Manager for postItem template */
+
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
